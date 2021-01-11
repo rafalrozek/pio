5be4ba24a1d789eb8737dcfda360d185e35d0189
kamil <kamil314bg@gmail.com>
2021-01-11 21:03:58 +0100

    dodanie "value" i "capacity"

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static int value = -1;
+    public static int capacity = 12;
 
-    public int total = -1;
+    private int[] numbers = new int[capacity];
+
+    public int total = value;
 
     public void countIn(int in) {
         if (!isFull())
