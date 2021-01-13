8cc2b79ca73505f2b9b8671d631973c547dc3136
rafalrozek <rafal98.poczta@gmail.com>
2021-01-13 15:14:54 +0100

    podpunkt 6, wygenerowanie komentarzy w klasie IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,6 +1,12 @@
 package edu.kis.vh.nursery.list;
-
+/**
+ * @Author Rafal Rozek
+ *
+ */
 public class IntLinkedList {
+    /**
+     *  Pomocnicza klasa zagniezdzona
+     */
     public class Node {
         public int value;
         public Node prev, next;
@@ -9,9 +15,14 @@ public class IntLinkedList {
         }
     }
 
-    Node last;
+    Node last; // ostatni element w liscie
     int i;
 
+    /**
+     *
+     * @param i - wartosc dodawana do listy
+     *
+     */
     public void push(int i) {
         if (last == null)
             last = new Node(i);
@@ -22,20 +33,40 @@ public class IntLinkedList {
         }
     }
 
+    /**
+     *
+     * @return true - jesli atrybut last jest rowny null
+     */
     public boolean isEmpty() {
         return last == null;
     }
 
+    /**
+     *
+     * @return false
+     *
+     */
     public boolean isFull() {
         return false;
     }
 
+    /**
+     *
+     * @return -1 - jesli lista jest pusta
+     * @return int - zwraca wartosc ostatnio dodanego elementu, jesli lista nie jest pusta
+     */
     public int top() {
         if (isEmpty())
             return -1;
         return last.value;
     }
 
+    /**
+     * Usuwa ostatni element w liscie
+     *
+     * @return -1 - jesli lista jest pusta
+     * @return int - zwraca wartosc usunietego elementu
+     */
     public int pop() {
         if (isEmpty())
             return -1;
